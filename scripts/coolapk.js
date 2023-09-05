409009460637ea0114024beb8961072b33644c37
Simon <48861499+simouse@users.noreply.github.com>
2023-09-05 13:20:35 +0800

    Update coolapk.js

--- scripts/coolapk.js
+++ scripts/coolapk.js
@@ -1,18 +1,23 @@
+/**
+ * 更新时间：2023 年 9 月 5 日 13 点 15 分
+ * 说明：此脚本只处理开屏广告，建议支持其他广告以便平台可持续运营。只在 shadowrocket 上测试过。
+ * 匹配模式：^https?:\/\/api\.coolapk\.com\/v6\/main\/init
+ */
+
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 function modifyInitResponse(obj) {
-  // Update: 2023-09-05
   obj.data = obj.data.map((item) => {
     // Use == in case property is string
     if (item.entityId == 944 || item.entityId == 945) {
-      // 热搜词条广告，比如 "巅峰极速" 和 "冒险岛"
+      // 热搜词条广告，比如 "巅峰极速" 和 "冒险岛"（规则已失效）
       return undefined;
-    } else if (item.cardId == 6390 || item.description?.includes('AdSlot')) {
+    } else if (item.cardId == 6390 || item.description?.includes('AdSlot') || item.extraDataArr?.["SplashAd.Expires"]) {
       // 单位是秒
       item.extraDataArr["Ad.SPLASH_RETRY_PERIOD"] = "2147483647";
-      item.extraDataArr["SplashAd.Expires"] = 2147483647; // 不知道是浮点数还是整数，0.001 必有广告
+      item.extraDataArr["SplashAd.Expires"] = 2147483647; // 不知道是浮点数还是整数，已知填 0.001 必有广告
       item.extraDataArr["SplashAd.onResume"] = "0";
       item.extraData = JSON.stringify(item.extraDataArr);
     }
@@ -20,51 +25,8 @@ function modifyInitResponse(obj) {
   }).filter((item) => item)
 }
 
-if (url.includes("/feed/detail")) {
-  if (obj.data?.hotReplyRows?.length > 0) {
-    obj.data.hotReplyRows = obj.data.hotReplyRows.filter((item) => item.id);
-  }
-  if (obj.data?.topReplyRows?.length > 0) {
-    obj.data.topReplyRows = obj.data.topReplyRows.filter((item) => item.id);
-  }
-  const item = ["detailSponsorCard", "include_goods", "include_goods_ids"];
-  for (let i of item) {
-    if (obj.data?.[i]) {
-      obj.data[i] = [];
-    }
-  }
-} else if (url.includes("/feed/replyList")) {
-  if (obj.data?.length > 0) {
-    obj.data = obj.data.filter((item) => item.id);
-  }
-} else if (url.includes("/main/dataList")) {
-  if (obj.data?.length > 0) {
-    obj.data = obj.data.filter(
-      (item) =>
-        !(item.entityTemplate === "sponsorCard" || item.title === "精选配件")
-    );
-  }
-} else if (url.includes("/main/indexV8")) {
-  if (obj.data?.length > 0) {
-    obj.data = obj.data.filter(
-      (item) =>
-        !(
-          item.entityTemplate === "sponsorCard" ||
-          item.entityId === 8639 ||
-          item.entityId === 29349 ||
-          item.entityId === 33006 ||
-          item.entityId === 32557 ||
-          item.title.includes("值得买") ||
-          item.title.includes("红包")
-        )
-    );
-  }
-} else if (url.includes("/main/init")) {
+if (url.includes("/main/init")) {
   modifyInitResponse(obj)
-} else if (url.includes("/page/dataList")) {
-  if (obj.data?.length > 0) {
-    obj.data = obj.data.filter((item) => !(item.title === "酷安热搜"));
-  }
 }
 
 $done({ body: JSON.stringify(obj) });
